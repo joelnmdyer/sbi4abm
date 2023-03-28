a02f80d8560969905fb23f1a4dfacaaeca2477a5
Joel Dyer <joelnmdyer@gmail.com>
2023-03-28 14:13:06 +0100

    Added some dependencies in setup.py

--- setup.py
+++ setup.py
@@ -9,3 +9,17 @@ setup(name='sbi4abm',
       license='AGPLv3',
       packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"])
 )
+
+REQUIRED = [
+    "joblib>=1.0.0",
+    "matplotlib",
+    "numpy",
+    "pillow",
+    "pyknos>=0.14.2",
+    "pyro-ppl>=1.3.1",
+    "scikit-learn",
+    "scipy",
+    "tensorboard",
+    "torch>=1.8.0",
+    "tqdm",
+]
